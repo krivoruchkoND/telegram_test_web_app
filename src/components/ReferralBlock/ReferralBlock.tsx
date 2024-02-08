4d38b5948fa80a49f0af8483e4c4baa248a77ae4
Nikita Kriivoruchko <krivoruchko.n@ati.su>
2024-02-08 21:06:12 +0300

    master: referralBlock draft

--- src/components/ReferralBlock/ReferralBlock.tsx
+++ src/components/ReferralBlock/ReferralBlock.tsx
@@ -1,5 +1,52 @@
+import rockVioletIcon from "@assets/RockViolet.svg";
+import copyActiveIcon from "@assets/CopyActive.svg";
+
+import classes from "./styles.module.css";
+
+const mock = {
+  referralsCount: 2,
+  rockEarned: "13.37K",
+  dollarEarned: "0.12",
+  referralLink: "https://t.me/rockbot?start=ref_mrct",
+};
+
 const ReferralBlock = () => {
-  return <div>ReferralBlock</div>;
+  return (
+    <div className={classes.referral}>
+      <div className={classes.title}>
+        <div className={classes.icon}>
+          <img src={rockVioletIcon} />
+        </div>
+        <div className={classes.text}>Referral system</div>
+      </div>
+      <div className={classes.counter}>
+        <span className={classes.title}>Referrals: </span>
+        <span className={classes.value}>{mock.referralsCount}</span>
+      </div>
+      <div className={classes.counter}>
+        <span className={classes.title}>$ROCK earned: </span>
+        <span className={classes.value}>
+          {mock.rockEarned} (${mock.dollarEarned})
+        </span>
+      </div>
+      <div className={classes.description}>
+        <p>
+          Rewards are updated at least every 24 hours and rewards are
+          automatically deposited to your ROCK balance.
+        </p>
+        <p>
+          Refer your friends and earn 30% of their fees in the first month. 20%
+          in the second and 10% forever!
+        </p>
+      </div>
+      <button className={classes.referralButton}>
+        <div className={classes.text}>{mock.referralLink}</div>
+        <div className={classes.icon}>
+          <img src={copyActiveIcon} />
+        </div>
+      </button>
+    </div>
+  );
 };
 
 export default ReferralBlock;
