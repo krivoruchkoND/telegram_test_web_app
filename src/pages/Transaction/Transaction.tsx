72065b6afff88321bb56d01fe7c0952ac4856827
Akim <savvinakim@gmail.com>
2024-03-09 16:19:10 +0300

    Added transaction button

--- src/pages/Transaction/Transaction.tsx
+++ src/pages/Transaction/Transaction.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { observer } from "mobx-react-lite";
-import { useLocation, useParams, Link } from "wouter";
+import { Link, useLocation, useParams } from "wouter";
 
 import useBackButton from "@hooks/useBackButton";
 import useRootStore from "@hooks/useRootStore";
@@ -9,6 +9,7 @@ import TransactionAction from "@components/TransactionAction";
 import FormItem from "@components/FormItem";
 
 import classes from "./styles.module.css";
+import TransactionButton from "@components/TransactionButton";
 
 const Transaction = () => {
   const [, setLocation] = useLocation();
@@ -41,18 +42,8 @@ const Transaction = () => {
       {currentTransaction && (
         <WalletTransactionItem transaction={currentTransaction} isOutOfList />
       )}
-      <TransactionAction action={action} onChange={setAction} />
 
-      <FormItem
-        id="slippage"
-        value={slippage}
-        onChange={setSlippage}
-        label="Slippage"
-        description="Difference between expected and actual results amounts of token"
-        inputMode="decimal"
-        placeholder="Enter value"
-        masks={["empty", "percent"]}
-      />
+      <TransactionAction action={action} onChange={setAction} />
 
       <FormItem
         id="amount"
@@ -67,6 +58,26 @@ const Transaction = () => {
           max: balance ?? 0,
         }}
       />
+
+      <FormItem
+        id="slippage"
+        value={slippage}
+        onChange={setSlippage}
+        label="Slippage"
+        description="Difference between expected and actual results amounts of token"
+        inputMode="decimal"
+        placeholder="Enter value"
+        masks={["empty", "percent"]}
+      />
+
+      <TransactionButton>
+        {
+          {
+            buy: "Buy ROCK",
+            sell: "Sell ROCK",
+          }[action]
+        }
+      </TransactionButton>
 
       {!isBackButtonSupported && <Link href="/">Go back</Link>}
     </section>
