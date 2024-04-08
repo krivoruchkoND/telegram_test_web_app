6e45f22dd3219d513c095d97285b5519ce115419
Nikita Kriivoruchko <krivoruchko.n@ati.su>
2024-04-08 20:25:15 +0300

    master: version trace

---
+++ src/global.d.ts
@@ -0,0 +1,3 @@
+interface Window {
+  __APP__: { version: string };
+}
--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,10 @@ import App from "./App.tsx";
 
 import "./index.css";
 
+window.__APP__ = {
+  version: "04-08-20:24-v1",
+};
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
