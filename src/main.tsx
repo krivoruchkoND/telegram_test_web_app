3b88f24ab5469947126fcb2da8f3ba53ec2b8693
Nikita Kriivoruchko <krivoruchko.n@ati.su>
2024-04-15 19:57:04 +0300

    master: version bump

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import App from "./App.tsx";
 import "./index.css";
 
 window.__APP__ = {
-  version: "04-08-20:24-v1",
+  version: "04-15-20:00-v1",
 };
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
